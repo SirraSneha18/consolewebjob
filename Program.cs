995caea8be4d8524d1ecc4971ea64deb482eaa7c
Sirra Sneha (LTIMINDTREE LIMITED) <v-sirrasneha@microsoft.com>
2025-04-22 15:22:33 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting Action1 Job...");
+
+        var profile = Environment.GetEnvironmentVariable("APP_PROFILE");
+        var dryRun = Environment.GetEnvironmentVariable("DRY_RUN");
+
+        Console.WriteLine($"APP_PROFILE: {profile}");
+        Console.WriteLine($"DRY_RUN: {dryRun}");
+
+        // Your WebJob logic here
+    }
+}
