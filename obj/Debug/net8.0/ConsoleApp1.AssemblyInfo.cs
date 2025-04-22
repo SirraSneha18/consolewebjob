e902a5861f36cc62def3cafa59fc319e242fc8fe
Sirra Sneha (LTIMINDTREE LIMITED) <v-sirrasneha@microsoft.com>
2025-04-22 17:34:25 +0530

    Include appsettings.json in publish output

--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+995caea8be4d8524d1ecc4971ea64deb482eaa7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74e6f256eba781e6308fe84d140b29367a574134")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
